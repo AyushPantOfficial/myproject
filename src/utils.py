6dfda8779f8b2a487742d0c6cee32046459dbdc4
Ayush Pant <ayupant@gmail.com>
2024-07-10 10:08:44 +0530

    Hyperparameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,8 @@ import dill
 
 from sklearn.metrics import r2_score
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
+
 
 def save_object(file_path,obj):
     try:
@@ -18,14 +20,21 @@ def save_object(file_path,obj):
         raise CustomException(e,sys)
         
 
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            # Training the model
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
+            # Training the model
+            #model.fit(X_train,y_train)
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
@@ -36,4 +45,13 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
         return report
     except Exception as e:
         raise CustomException(e,sys)
+    
+def load_obj(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
 
